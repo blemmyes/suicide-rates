948a5517e4ed9d902ba87ad0e2b1ec0bdc99a50a
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-12-13 06:37:53 +0100

    Shiny App: added dotplot (number of years covered by country)

--- suicide-app.R
+++ suicide-app.R
@@ -10,7 +10,6 @@ read_suicide_data <- function(csv_file_path) {
 }
 
 height <- '1024px'
-
 ui <- navbarPage('DASB: Suicide Rates', id='tabs',
                  tabPanel('Overview',
                           sidebarLayout(sidebarPanel(h1('Overview'),
@@ -19,7 +18,11 @@ ui <- navbarPage('DASB: Suicide Rates', id='tabs',
                  tabPanel('Histogram',
                           sidebarLayout(sidebarPanel(h1('Histogram'),
                                                      p('The histogram on the right hand side...')),
-                                        mainPanel(plotOutput(outputId='histogram', height=height))))
+                                        mainPanel(plotOutput(outputId='histogram', height=height)))),
+                 tabPanel('Country Data',
+                          sidebarLayout(sidebarPanel(h1('Country Data'),
+                                                    p('The number of data per country and year...')),
+                                       mainPanel(plotOutput(outputId='countryData', height=height))))
 )
 
 data <- read_suicide_data('./data/master.csv')
@@ -28,15 +31,20 @@ by_country <- by_country_year %>% group_by(country) %>% summarize(count=n())
 
 server <- function(input, output) {
     max <- max(by_country$count)
+    output$overview <- renderPlot(ggplot(data=by_country_year, mapping=aes(x=year, y=country)) +
+                                  geom_tile() +
+                                  xlab('Year') +
+                                  ylab('Country'))
     output$histogram <- renderPlot(ggplot(data=by_country, mapping=aes(count)) +
                                    scale_x_continuous(breaks = seq(1, max, 1)) +
                                    geom_histogram(binwidth=1) +
                                    xlab('Number of Years Covered') +
                                    ylab('Number of Countries'))
-    output$overview <- renderPlot(ggplot(data=by_country_year, mapping=aes(x = year, y = country)) +
-                                  geom_tile() +
-                                  xlab('Year') +
-                                  ylab('Country'))
+    output$countryData <- renderPlot(ggplot(data=by_country, mapping=aes(x=country, y=count)) +
+                                     geom_point() +
+                                     theme(axis.text.x=element_text(angle=90, hjust=1, vjust=+0.25)) +
+                                     xlab('Country') +
+                                     ylab('Number of Years Covered'))
 }
 
 shinyApp(ui=ui, server=server, options=list(port=1337))
