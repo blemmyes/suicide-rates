363b5f4dab5da3d2908dadf0f05011e23fa185d6
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-12-13 06:45:20 +0100

    Shiny App: added GDP per Capita plot

--- suicide-app.R
+++ suicide-app.R
@@ -22,12 +22,23 @@ ui <- navbarPage('DASB: Suicide Rates', id='tabs',
                  tabPanel('Country Data',
                           sidebarLayout(sidebarPanel(h1('Country Data'),
                                                     p('The number of data per country and year...')),
-                                       mainPanel(plotOutput(outputId='countryData', height=height))))
+                                       mainPanel(plotOutput(outputId='countryData', height=height)))),
+                 tabPanel('GDP per Capita',
+                          sidebarLayout(sidebarPanel(h1('GDP per Capita'),
+                                                     p('This plot shows...')),
+                                        mainPanel(plotOutput(outputId='gdpPerCapita', height=height))))
 )
 
 data <- read_suicide_data('./data/master.csv')
 by_country_year <- data %>% group_by(country, year) %>% summarize(n())
 by_country <- by_country_year %>% group_by(country) %>% summarize(count=n())
+suicide_gdp <- data %>%
+    group_by(country, year, suicides.100k.pop, gdp_per_capita....) %>%
+    summarize(n()) %>%
+    group_by(country, year) %>%
+    mutate(total_suicides_100k_pop=sum(suicides.100k.pop)) %>%
+    group_by(country, year, total_suicides_100k_pop, gdp_per_capita....) %>%
+    summarize(count=n())
 
 server <- function(input, output) {
     max <- max(by_country$count)
@@ -36,7 +47,7 @@ server <- function(input, output) {
                                   xlab('Year') +
                                   ylab('Country'))
     output$histogram <- renderPlot(ggplot(data=by_country, mapping=aes(count)) +
-                                   scale_x_continuous(breaks = seq(1, max, 1)) +
+                                   scale_x_continuous(breaks=seq(1, max, 1)) +
                                    geom_histogram(binwidth=1) +
                                    xlab('Number of Years Covered') +
                                    ylab('Number of Countries'))
@@ -45,6 +56,13 @@ server <- function(input, output) {
                                      theme(axis.text.x=element_text(angle=90, hjust=1, vjust=+0.25)) +
                                      xlab('Country') +
                                      ylab('Number of Years Covered'))
+    output$gdpPerCapita <- renderPlot(ggplot(data=suicide_gdp,
+                                             mapping=aes(x=gdp_per_capita...., y=total_suicides_100k_pop)) +
+                                      geom_point(alpha=0.25) +
+                                      theme(legend.position='none') +
+                                      geom_smooth() +
+                                      xlab('GDP per Capita') +
+                                      ylab('Suicides (per 100k Inhabitants)'))
 }
 
 shinyApp(ui=ui, server=server, options=list(port=1337))
