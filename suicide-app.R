57032c06d5a183f12d069adfb69e961bf61e90ea
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-12-13 06:29:22 +0100

    Shiny App: tab based layout with side bars; overview and histogram plot

--- suicide-app.R
+++ suicide-app.R
@@ -9,28 +9,34 @@ read_suicide_data <- function(csv_file_path) {
     return(tbbl)
 }
 
-ui <- fluidPage(titlePanel('DASB: Suicide Rates'),
-                fluidRow(column(12, sliderInput(inputId='minMeasurements',
-                                                label='Min. Measurements',
-                                                min=1,
-                                                max=100, # TODO: highest from data set
-                                                value=10)),
-                         column(6, plotOutput(outputId='histogram')))
-)
+height <- '1024px'
 
-# TODO: filter by input$minMeasurements
+ui <- navbarPage('DASB: Suicide Rates', id='tabs',
+                 tabPanel('Overview',
+                          sidebarLayout(sidebarPanel(h1('Overview'),
+                                                     p('The diagram on the right hand side shows...')),
+                                        mainPanel(plotOutput(outputId='overview', height=height)))),
+                 tabPanel('Histogram',
+                          sidebarLayout(sidebarPanel(h1('Histogram'),
+                                                     p('The histogram on the right hand side...')),
+                                        mainPanel(plotOutput(outputId='histogram', height=height))))
+)
 
 data <- read_suicide_data('./data/master.csv')
 by_country_year <- data %>% group_by(country, year) %>% summarize(n())
 by_country <- by_country_year %>% group_by(country) %>% summarize(count=n())
 
 server <- function(input, output) {
-    observeEvent(input$minMeasurements, {
-        output$histogram <- renderPlot(ggplot(data=by_country, mapping=aes(count)) +
-                                       # TODO: replace 35 by...
-                                       scale_x_continuous(breaks = seq(1, 35, 1)) +
-                                       geom_histogram(binwidth=1))
-    })
+    max <- max(by_country$count)
+    output$histogram <- renderPlot(ggplot(data=by_country, mapping=aes(count)) +
+                                   scale_x_continuous(breaks = seq(1, max, 1)) +
+                                   geom_histogram(binwidth=1) +
+                                   xlab('Number of Years Covered') +
+                                   ylab('Number of Countries'))
+    output$overview <- renderPlot(ggplot(data=by_country_year, mapping=aes(x = year, y = country)) +
+                                  geom_tile() +
+                                  xlab('Year') +
+                                  ylab('Country'))
 }
 
 shinyApp(ui=ui, server=server, options=list(port=1337))
